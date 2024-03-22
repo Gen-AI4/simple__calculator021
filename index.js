920000825ea5e77f6758717742ecc179fc209cc9
Ahmed Ali <160950887+Gen-AI4@users.noreply.github.com>
2024-03-23 01:59:05 +0500

    make simple calculator

---
+++ index.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        message: "Enter your first number", type: "number", name: "firstNumbr"
+    },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
+    { message: "select one of the operation + - * /", type: "list", name: "operation", choices: ["+", "-", "*", "/"],
+    },
+]);
+if (answer.operation === "+") {
+    console.log(answer.firstNumbr + answer.secondNumber);
+}
+else if (answer.operation === "-") {
+    console.log(answer.firstNumbr - answer.secondNumber);
+}
+else if (answer.operation === "*") {
+    console.log(answer.firstNumbr * answer.secondNumber);
+}
+else if (answer.operation === "/") {
+    console.log(answer.firstNumbr / answer.secondNumber);
+}
+else {
+    console.log("Invalid operation");
+}
+;
